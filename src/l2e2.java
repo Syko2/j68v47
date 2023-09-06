0aecc28f465d23182039c649cf4a72373252327e
30124856 <30124856@nescol.ac.uk>
2023-09-06 11:34:38 +0100

    getting everything backed up

--- src/l2e2.java
+++ src/l2e2.java
@@ -5,6 +5,22 @@ public class l2e2 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.print("What is the first number? ");
+        int num1 = input.nextInt();
         System.out.print("What is the second number? ");
-        int num2 = (input.nextint());
+        int num2 = input.nextInt();
+
+        int addans = num1 + num2;
+        System.out.print((num1)+ "+" +(num2) + "=" + (addans)+"\n");
+        int minans = num1 - num2;
+        System.out.print((num1)+ "-" +(num2) + "=" + (minans)+"\n");
+        int mulans = num1 * num2;
+        System.out.print((num1)+ "x" +(num2) + "=" + (mulans)+"\n");
+        int divans = num1 / num2;
+        System.out.print((num1)+ "/" +(num2) + "=" + (divans)+"\n");
+        float percans = num1 % num2;
+        System.out.print((num1)+ "%" +(num2) + "=" + (percans)+"\n");
+        double sqaans = Math.pow(num1,num2);
+        System.out.print((num1)+ "^" +(num2) + "=" + (sqaans)+"\n");
+
     }
+}
