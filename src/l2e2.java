edc67aeeecde28c30750611436a85a13e15213c5
30124856 <30124856@nescol.ac.uk>
2023-09-06 10:41:03 +0100

    getting everything backed up

---
+++ src/l2e2.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+
+public class l2e2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("What is the first number? ");
+        System.out.print("What is the second number? ");
+        int num2 = (input.nextint());
+    }
